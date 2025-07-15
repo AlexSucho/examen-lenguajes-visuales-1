a412749be9485227c7c14397ae2109283a81d1de
Roger Lopez <alexyosoy49@gmail.com>
2025-07-15 20:03:29 -0300

    Delete examen-programacion-1 directory

--- examen-programacion-1/examen-programación-1/src/main/java/com/rogerlopez/examen_programacion_1/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rogerlopez.examen_programacion_1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.rogerlopez.examen_programacion_1.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
